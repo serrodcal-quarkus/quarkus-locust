162ba4d8d5761ff9d5c60ce9cea003c21106c5af
SERGIO RODRIGUEZ CALVO <SERGIO.RODRIGUEZ.CALVO@ing.com>
2021-01-31 14:44:58 +0100

    add log statement, new dashboard and modify resources

--- src/main/java/com/serrodcal/poc/GreetingResource.java
+++ src/main/java/com/serrodcal/poc/GreetingResource.java
@@ -6,16 +6,20 @@ import io.vertx.core.http.HttpMethod;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
+import org.jboss.logging.Logger;
 
 import javax.inject.Singleton;
 
 @Singleton
 public class GreetingResource {
 
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class);
+
     @Route(path = "/hello", methods = HttpMethod.GET)
     @Counted(name = "performedChecks", description = "How many primality checks have been performed.")
     @Timed(name = "checksTimer", description = "A measure of how long it takes to perform the primality test.", unit = MetricUnits.MILLISECONDS)
     public Uni<String> hello() {
+        this.LOG.info("hello!");
         return Uni.createFrom().item("Hello!");
     }
     
